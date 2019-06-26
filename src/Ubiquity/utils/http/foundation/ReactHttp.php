96f6aa57a3d87660773cecde7a6a7cf59d4a79ee
jcheron <myaddressmail@gmail.com>
2019-06-26 03:11:57 +0200

    Fix header(replace) default value

--- src/Ubiquity/utils/http/foundation/ReactHttp.php
+++ src/Ubiquity/utils/http/foundation/ReactHttp.php
@@ -1,33 +1,36 @@
 <?php
-
 namespace Ubiquity\utils\http\foundation;
 
 /**
  * Http instance for ReactPHP.
  * Ubiquity\utils\http\foundation$ReactHttp
  * This class is part of Ubiquity
+ *
  * @author jcheron <myaddressmail@gmail.com>
  * @version 1.0.0
- *
+ *         
  */
 class ReactHttp extends AbstractHttp {
+
 	private $headers;
-	private $responseCode=200;
+
+	private $responseCode = 200;
+
 	private $request;
-	
-	
+
 	public function getAllHeaders() {
 		return $this->headers;
 	}
 
-	public function header($key,$value,$replace=null,$http_response_code=null) {
-		$this->headers[$key]=$value;
-		if($http_response_code!=null){
-			$this->responseCode=$http_response_code;
+	public function header($key, $value, bool $replace = true, int $http_response_code = null) {
+		$this->headers[$key] = $value;
+		if ($http_response_code != null) {
+			$this->responseCode = $http_response_code;
 		}
 	}
-	
+
 	/**
+	 *
 	 * @return int
 	 */
 	public function getResponseCode() {
@@ -35,41 +38,48 @@ class ReactHttp extends AbstractHttp {
 	}
 
 	/**
+	 *
 	 * @param mixed $headers
 	 */
 	private function setHeaders($headers) {
-		foreach ($headers as $k=>$header){
-			if(is_array($header) && sizeof($header)==1){
+		foreach ($headers as $k => $header) {
+			if (is_array($header) && sizeof($header) == 1) {
 				$this->headers[$k] = current($header);
-			}else{
-				$this->headers[$k]=$header;
+			} else {
+				$this->headers[$k] = $header;
 			}
 		}
 	}
 
 	/**
+	 *
 	 * @param int $responseCode
 	 */
 	public function setResponseCode($responseCode) {
-		if($responseCode!=null){
+		if ($responseCode != null) {
 			$this->responseCode = $responseCode;
 		}
 	}
-	
+
 	public function headersSent(string &$file = null, int &$line = null) {
 		return false;
 	}
-	
+
 	public function getInput() {
 		return $this->request->getParsedBody();
 	}
+
 	/**
+	 *
 	 * @param mixed $request
 	 */
 	public function setRequest($request) {
-		$this->request=$request;
-		$default=['Content-Type'=>'text/html; charset=utf-8','Server'=>$request->getUri()->getHost()];
-		$this->setHeaders(array_merge($default,$request->getHeaders()));
+		$this->request = $request;
+		$default = [
+			'Content-Type' => 'text/html; charset=utf-8',
+			'Server' => $request->getUri()->getHost()
+		];
+		$this->setHeaders(array_merge($default, $request->getHeaders()));
 	}
 }
 
